c2799e3c1c298f6f64c19c0c00a36bebc8174032
ThemisBk <98816715+ThemisBk@users.noreply.github.com>
2026-02-18 14:59:38 +0200

    20260214132632_remove_bad_foreign_keys_from_friends.rb

--- db/migrate/20260214132632_remove_bad_foreign_keys_from_friends.rb
+++ db/migrate/20260214132632_remove_bad_foreign_keys_from_friends.rb
@@ -1,6 +1,5 @@
 class RemoveBadForeignKeysFromFriends < ActiveRecord::Migration[8.1]
   def change
-    # Remove the problematic foreign keys
     remove_foreign_key :friends, column: :friend_id rescue nil
     remove_foreign_key :friends, column: :user_id rescue nil
   end
